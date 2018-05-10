f8b3c8440088cbd081197e0d070b46e1dbf29978
Mahendra Kumar <mahendrakumar@mahendras-mbp.home>
2018-05-10 20:48:23 +0200

    adding posts and form component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Posts from './Post'
+import PostForm from './PostForm';
 
 export default class App extends Component{
     render() {
         return(
-            <div> 
-               <h1>My React App</h1>
+            <div>
+              <PostForm/>
+              <hr/>
+              <Posts/>
             </div>
         )
             
